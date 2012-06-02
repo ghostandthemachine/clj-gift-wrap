0bc21124687e7696f34fab3a3e39d26e16262f5c
Jon <jonrose08@gmail.com>
2012-06-01 23:29:35 -0700

    clean up syntax

--- src/clj_gift_wrap/core.clj
+++ src/clj_gift_wrap/core.clj
@@ -8,33 +8,56 @@
 		  [clojure.string :as s]
 		  [clojure.contrib.string :as cs]))
 
-(def output (atom ""))
+(def alphabet ["a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z"])
+(def global-class-name (atom nil))
 
 (defn in? 
   "true if seq contains elm"
   [seq elm]  
   (some #(= elm %) seq))
 
+(defn single-boolean-arg?
+	[fn-name]
+	(= "boolean" (str (s/join (take 7 (drop 1 (str fn-name)))))))
+
+(def t {:parameter-types "[boolean]" :return-type "boolean"})
+
+(defn boolean? 
+	[m]
+	(or 
+		(single-boolean-arg? (:parameter-types m))
+		(= "boolean" (str (s/join (take 7 (str (:return-type m))))))))
+
+(defn setter?
+	[fn-name]
+	(= "set" (str (s/join (take 3 (str fn-name))))))
+
+(defn getter?
+	[m]
+	(and
+		(not (boolean? m))
+		(= "get" (str (s/join (take 3 (str (:name m))))))))
+
 (defn get-fn-type 
 	[m]
 	(cond
-		(= "get" (str (s/join (take 3 (str (:name m))))))
+	  (getter? m)
 	  :getter
-	  (= "set" (str (s/join (take 3 (str (:name m))))))
+	  (and
+	  	(setter? (:name m))
+	  	(not (single-boolean-arg? (:parameter-types m))))
 	  :setter
-	  (= (str (:return-type m)) "boolean")
-	  :boolean))
-
-(def alphabet ["a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z"])
+	  (or 
+	  	(boolean? m)
+	  	(single-boolean-arg? (:parameter-types m)))
+	  :boolean
+	  :else 
+	  	:unknown))
 
 (defn parse-args
 	[arguments]
 	(let [num-args (count arguments)]
 		(apply str (for [i (range num-args)] (str " " (nth alphabet i))))))
-
-; (defn parse-args
-; 	[arguments]
-; 	(apply str (interleave " " arguments)))
 
 (defn public? 
 	[m]
@@ -46,26 +69,62 @@
 	[m]
 	(in? (:flags m) :static))
 
+(defn synthetic?
+	[m]
+	(in? (:flags m) :synthetic))
+
+(defn format-setter
+	[method-name]
+	(str (s/join (drop 4 (hyphenize method-name))) "!"))
+
+(defn format-getter
+	[method-name]
+	(str (s/join (drop 4 (hyphenize method-name)))))
+
+(defn format-boolean 
+	[m]
+
+	(let [method-name (str (:name m))]
+		(cond
+	  		(= "is" (str (s/join (take 2 method-name))))
+				(str (hyphenize (str (s/join (drop 2 method-name)))) "?")
+	  		(= "has" (str (s/join (take 3 method-name))))
+				(str (hyphenize (str (s/join (drop 3 method-name)))) "?")
+	  		(= "set" (str (s/join (take 3 method-name))))
+				(str (hyphenize (str (s/join (drop 3 method-name)))) "?")
+			(and 
+				(boolean? m)
+				(= "get" (str (s/join (take 3 method-name)))))
+				(str (hyphenize (str (s/join (drop 3 method-name)))) "?")
+			:else
+				(str (hyphenize (str method-name)) "?"))))
+
+(defn format-unkown
+	[method-name]
+	(str (hyphenize method-name)))
+
 (defn parse-fn-name
 	[m]
 	(let [type (get-fn-type m)
 		  	name (cond
 	  				(= type :setter)
-	  				(str (s/join (drop 4 (hyphenize (str (:name m))))) "!")
+	  					(format-setter (str (:name m)))
 	  				(= type :getter)
-	  				(str (s/join (drop 4 (hyphenize (str (:name m))))))
+	  					(format-getter (str (:name m)))
 	  				(= type :boolean)
-						(str (hyphenize (str (:name m))) "?")
-	  				:else
-	  				(hyphenize (str (:name m))))]
+						(format-boolean m)
+	  				(= type :unkown)
+	  					(format-unkown (str (:name m)))
+	  				:else 
+	  					(format-unkown (str (:name m))))]
  		(str name "\n")))
 		
 (defn public-method-to-string 
 	[m]
 	(str "(defn " (parse-fn-name m)
-		"\"args " (str (:parameter-types m))"\"\n"
-		"\"flags " (str (:flags m))"\"\n"
-		"\"return-type " (str (:return-type m))"\"\n"
+		"\"args: " (str (:parameter-types m))"\n"
+		"flags: " (str (:flags m))"\n"
+		"return-type: " (str (:return-type m))"\"\n"
 		"\t[obj" (parse-args (:parameter-types m)) "]\n"
 		"\t(." (str (:name m)) " obj" (parse-args (:parameter-types m)) "))\n\n"))
 
@@ -73,34 +132,35 @@
 	[m class-name]
 	(if (public? m)
 		(str "(defn " (parse-fn-name m)
-		"\"args " (str (:parameter-types m))"\"\n"
-		"\"flags " (str (:flags m))"\"\n"
-		"\"return-type " (str (:return-type m))"\"\n"
+		"\"args: " (str (:parameter-types m))"\n"
+		"flags: " (str (:flags m))"\n"
+		"return-type: " (str (:return-type m))"\"\n"
 		"\t[obj" (parse-args (:parameter-types m)) "]\n"
 		"\t(" (:declaring-class m) "/" (str (:name m)) (parse-args (:parameter-types m)) "))\n\n")))
-
-(def holder (atom nil))
 
 (defn parse-method
 	[method]
 	(let [m method
-		  class-name @holder]
+		  class-name @global-class-name]
 		(cond 
 			(and 
 				(public? m)
-				(not (static? m)))
+				(not (static? m))
+				(not (synthetic? m)))
 			(public-method-to-string m)
-			(static? m)
+			(and 
+				(static? m)
+				(not (synthetic? m)))
 			(static-method-to-string m class-name)
 			:else 
 			"")))
 
 (defn build-wrapper
 	[obj]
-	(swap! holder (fn [_] (s/join (drop 6 (str (type obj))))))
+	(swap! global-class-name (fn [_] (s/join (drop 6 (str (type obj))))))
 	(let [members (:members (reflect obj))
-		  header (str "(ns " (cs/replace-char \. \_ (underscore @holder)) ".core)\n\n\n")
-		  file-name (str (cs/replace-char \. \_ (underscore @holder)) ".clj")]
+		  header (str "(ns " (cs/replace-char \. \_ (underscore @global-class-name)) ".core)\n\n\n")
+		  file-name (str (cs/replace-char \. \_ (underscore @global-class-name)) ".clj")]
 		(->> members
 			(map parse-method)  
   			(apply str)
@@ -108,10 +168,6 @@
 
 	(let [original (slurp file-name)]
 		(spit file-name (str header original)))))
-
-; (build-wrapper java.lang.Integer)
-
-
 
 
 (defn -main [& args]
@@ -127,7 +183,7 @@
 
 
 
-
+(def m (:name (first (:members (reflect (RSyntaxTextArea. ))))))
 
 
 
